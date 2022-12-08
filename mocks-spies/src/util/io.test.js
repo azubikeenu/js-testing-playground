32f3016ea1292329feae1e3781aa1a97bc916a85
Enu Azubike <enuazubike88@gmail.com>
2022-12-08 20:24:47 +0100

    minor updates

--- mocks-spies/src/util/io.test.js
+++ mocks-spies/src/util/io.test.js
@@ -1,12 +1,13 @@
 import { it, expect, vi } from 'vitest';
 import { promises as fs } from 'fs';
+import path from 'path';
 import writeData from './io';
 
 vi.mock('fs');
 
 vi.mock('path', () => {
   return {
-    default: {
+    default: {  // the default key is added because we used the default export in the path native function
       join: (...args) => {
         return args[args.length - 1];
       },
@@ -33,4 +34,14 @@ it('should return a promise that resolves to undefined ', () => {
   const testData = 'dummy data';
   const fileName = 'data.txt';
   expect(writeData(testData, fileName)).resolves.toBeUndefined();
+});
+
+it('should execute the write file method [using spyOn]', () => {
+  const testData = 'dummy data';
+  const fileName = 'data.txt';
+  vi.spyOn(path , 'join').mockReturnValue(fileName)
+  const fsMock = vi.spyOn(fs, 'writeFile')
+  writeData(testData, fileName);
+  expect(fsMock).toHaveBeenCalled();
+  expect(fsMock).toHaveBeenCalledWith(fileName, testData);
 });
